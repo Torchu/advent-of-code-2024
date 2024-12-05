af3cb5ad6d672b979e31c128111309b52ce2ab62
torchu <victor.cabritag@gmail.com>
2024-12-05 10:56:14 +0100

    d5p1: check validity of updates

--- src/5-print-queue/index.ts
+++ src/5-print-queue/index.ts
@@ -27,10 +27,32 @@ const readInput = async (file: string): Promise<Data> => {
   return { rules, updates };
 };
 
+/**
+ * Checks if the update is valid.
+ *
+ * @param rules - The rules to check the update against.
+ * The key is the number to check and the values the numbers that cannot appear before the key.
+ * @param update - The update to check.
+ *
+ * @returns True if the update is valid, false otherwise.
+ */
+const isValidUpdate = (
+  rules: Record<number, number[]>,
+  update: number[]
+): boolean => {
+  return update.every((n: number, idx: number) => {
+    if (!rules[n]) return true;
+    const previous = update.slice(0, idx);
+    return rules[n].every((rule) => !previous.includes(rule));
+  });
+};
+
 // MAIN
 const main = async (file: string) => {
   const data = await readInput(file);
-  console.log(data);
+  data.updates.forEach((update) => {
+    console.log(update, isValidUpdate(data.rules, update));
+  });
 };
 
 export default main;
