128bc359633ea9e8968d9d5fd9fbfcd2d3a1131f
torchu <victor.cabritag@gmail.com>
2024-12-05 11:55:38 +0100

    d5p2: rearrange updates validation and fixes invalid updates

--- src/5-print-queue/index.ts
+++ src/5-print-queue/index.ts
@@ -28,6 +28,30 @@ const readInput = async (file: string): Promise<Data> => {
 };
 
 /**
+ * Validate the updates using a ruleset
+ *
+ * @param rules - Rules to check the updates against
+ * @param updates Updates to check
+ *
+ * @returns A dictionary with the valid and invalid updates
+ */
+const validateUpdates = (
+  rules: Record<number, number[]>,
+  updates: number[][]
+): { validUpdates: number[][]; invalidUpdates: number[][] } => {
+  const validUpdates: number[][] = [];
+  const invalidUpdates: number[][] = [];
+  updates.forEach((update) => {
+    if (isValidUpdate(rules, update)) {
+      validUpdates.push(update);
+    } else {
+      invalidUpdates.push(update);
+    }
+  });
+  return { validUpdates, invalidUpdates };
+};
+
+/**
  * Checks if the update is valid.
  *
  * @param rules - The rules to check the update against.
@@ -50,26 +74,57 @@ const isValidUpdate = (
 /**
  * Sum the middle number of all valid updates.
  *
- * @param rules - The rules to check the update against.
- * @param updates - The updates to check.
+ * @param updates - The updates to sum.
  *
- * @returns The sum of the middle number of all valid updates.
+ * @returns The sum of the middle number of the updates.
  */
-const sumValidUpdates = (
-  rules: Record<number, number[]>,
-  updates: number[][]
-): number => {
-  const validUpdates = updates.filter((update) => isValidUpdate(rules, update));
-  return validUpdates.reduce(
+const sumUpdates = (updates: number[][]): number => {
+  return updates.reduce(
     (acc, update) => acc + update[Math.floor(update.length / 2)],
     0
   );
 };
 
+/**
+ * Fixes the update by reordering the numbers according to the rules.
+ *
+ * @param rules - Rules to fix the update.
+ * @param update - Update to fix.
+ *
+ * @returns The fixed update.
+ */
+const fixUpdate = (
+  rules: Record<number, number[]>,
+  update: number[]
+): number[] => {
+  let result = [...update];
+  do {
+    for (let i = 0; i < result.length; i++) {
+      if (!rules[result[i]]) continue;
+      for (let j = 0; j < i; j++) {
+        if (rules[result[i]].includes(result[j])) {
+          const tmp = result[j];
+          result[j] = result[i];
+          result[i] = tmp;
+        }
+      }
+    }
+  } while (!isValidUpdate(rules, result));
+  return result;
+};
+
 // MAIN
 const main = async (file: string) => {
   const data = await readInput(file);
-  console.log(sumValidUpdates(data.rules, data.updates));
+  const { validUpdates, invalidUpdates } = validateUpdates(
+    data.rules,
+    data.updates
+  );
+  console.log(sumUpdates(validUpdates));
+
+  console.log(
+    sumUpdates(invalidUpdates.map((update) => fixUpdate(data.rules, update)))
+  );
 };
 
 export default main;
