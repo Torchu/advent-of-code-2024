680ed623e8cdb20963c53bb508ea6681ae433579
torchu <victor.cabritag@gmail.com>
2024-12-10 10:58:23 +0100

    d6p1: calculate guard route and count the positions visited

--- src/6-guard-gallivant/index.ts
+++ src/6-guard-gallivant/index.ts
@@ -64,6 +64,37 @@ const readInput = async (
 };
 
 /**
+ * Move the guard to the next position
+ *
+ * @param map Map of the area
+ * @param position Current position of the guard
+ *
+ * @returns New position of the guard
+ */
+const move = (map: string[][], position: Position): Position => {
+  const { x, y, direction } = position;
+  let newPosition: Position | undefined = undefined;
+  switch (direction) {
+    case Direction.UP:
+      newPosition = { x, y: y - 1, direction };
+      break;
+    case Direction.RIGHT:
+      newPosition = { x: x + 1, y, direction };
+      break;
+    case Direction.DOWN:
+      newPosition = { x, y: y + 1, direction };
+      break;
+    default: // Direction.LEFT
+      newPosition = { x: x - 1, y, direction };
+      break;
+  }
+  if (isObstacle(map, newPosition.x, newPosition.y)) {
+    newPosition = { x, y, direction: (direction + 1) % 4 };
+  }
+  return newPosition;
+};
+
+/**
  * Check if the given position is an obstacle
  *
  * @param map Map of the area
@@ -73,6 +104,9 @@ const readInput = async (
  * @returns True if the position is an obstacle, false otherwise
  */
 const isObstacle = (map: string[][], x: number, y: number): boolean => {
+  if (isOutOfBounds(map, x, y)) {
+    return false;
+  }
   return map[y][x] === "#";
 };
 
@@ -89,15 +123,44 @@ const isOutOfBounds = (map: string[][], x: number, y: number): boolean => {
   return map.length <= y || map[0].length <= x || x < 0 || y < 0;
 };
 
+/**
+ * Completes the patrol route of the guard and returns the positions visited
+ *
+ * @param map Map of the area
+ * @param initialPosition Initial position of the guard
+ *
+ * @returns The list of positions visited by the guard
+ */
+const patrol = (map: string[][], initialPosition: Position): Position[] => {
+  let currentPosition = { ...initialPosition };
+  const visitedPositions: Position[] = [];
+  while (!isOutOfBounds(map, currentPosition.x, currentPosition.y)) {
+    visitedPositions.push(currentPosition);
+    currentPosition = move(map, currentPosition);
+  }
+  return visitedPositions;
+};
+
+/**
+ * Count the number of unique positions in the given route
+ *
+ * @param route Route of the guard
+ *
+ * @returns The number of unique positions in the route
+ */
+const countPositions = (route: Position[]): number => {
+  const positions = new Set<string>();
+  route.forEach((position) => {
+    positions.add(`${position.x},${position.y}`);
+  });
+  return positions.size;
+};
+
 // MAIN
 const main = async (file: string) => {
   const data = await readInput(file);
-  console.log(data);
-  console.log(isObstacle(data.map, 0, 0));
-  console.log(isObstacle(data.map, 4, 0));
-  console.log(isOutOfBounds(data.map, 0, 0));
-  console.log(isOutOfBounds(data.map, -1, 0));
-  console.log(isOutOfBounds(data.map, 0, 10));
+  const route = patrol(data.map, data.initialPosition);
+  console.log(countPositions(route));
 };
 
 export default main;
